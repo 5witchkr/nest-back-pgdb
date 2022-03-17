b29ed4bc3660cc85b92cf17e084e080057b977f6
5witchkr <hob4419@gmail.com>
2022-03-17 22:11:22 +0900

    authModule & userEntity,userRepository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { ContentsModule } from './contents/contents.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    ContentsModule
+    ContentsModule,
+    AuthModule
   ],
 })
 export class AppModule {}
