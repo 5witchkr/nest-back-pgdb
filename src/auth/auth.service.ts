f86990c1a98a8ea0c8dedf294652c83ec17c984f
5witchkr <hob4419@gmail.com>
2022-03-18 23:13:06 +0900

    jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from  'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private jwtService: JwtService
     ) {}
     //회원가입
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
@@ -16,13 +18,17 @@ export class AuthService {
     }
 
     //로그인
-    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         const { username, password} = authCredentialsDto;
         const user = await this.userRepository.findOne({ username });
 
         //bcrypt compare로 비밀번호 대조
         if (user && (await bcrypt.compare(password, user.password))) {
-            return 'login success';
+            //유저 토큰생성 (secret + payload)
+            const payload = { username };
+            const accessToken = await this.jwtService.sign(payload);
+
+            return { accessToken };
         } else {
             throw new UnauthorizedException('login failed');
         }
