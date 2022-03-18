06fb6947f1bc8f4912c08dd093c7a2a092327891
5witchkr <hob4419@gmail.com>
2022-03-18 22:08:43 +0900

    bcryptjs - hased password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,19 @@ import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
        async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
            const { username, password } = authCredentialsDto;
-           const user = this.create({ username, password});
+
+           //salt + hash 사용하여 암호화
+           const salt = await bcrypt.genSalt();
+           const hashedPassword = await bcrypt.hash(password, salt);
+
+
+           const user = this.create({ username, password: hashedPassword});
             //저장 및 error.code 23505(동일유저id) 에러캐치처리
            try {
                await this.save(user);
