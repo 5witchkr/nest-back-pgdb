800ab9a5c35594162eb7e939134dc4f16b9b4bdb
5witchkr <hob4419@gmail.com>
2022-03-20 01:03:17 +0900

    config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,8 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { User } from "./user.entity";
 import { UserRepository } from "./user.repository";
+import * as config from 'config';
+
 
 //jwt strategy
 @Injectable()
@@ -13,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         private userRepository: UserRepository
     ) {
         super({
-            secretOrKey: 'switch1234',
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
         })
     }
